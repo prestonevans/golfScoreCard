40654b9bdc971e096073ffe1cd3b6b07237219da
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-07 16:36:11 -0700

    made createPlayers function to make player rows depending on user input

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const form = document.querySelector('form');
 //grab table to append data
 const table = document.querySelector('table');
 
+// grab player template
+const playerTemplate = document.querySelector('#playerContent');
+
 //event listener to prevent the default of a form being submitted
 form.addEventListener('submit', (e) => {
 	// prevent form from default action
@@ -20,13 +23,16 @@ form.addEventListener('submit', (e) => {
 	const teeSelect = document.querySelector('#teeSelect');
 	const teeSelectInput = teeSelect.options[teeSelect.selectedIndex].value;
 
-	//Creates holes row
+	// creates holes row
 	createHoles();
 
-	// rended data from api
+	// create yards,hcp, and par rows from api
+	// creat players after api rows are created
 	coursePromise.then((data) => {
 		const course = data.data;
 		createCourse(course, teeSelectInput);
+		//create player rows
+		createPlayers(numberOfPlayersInput);
 	});
 });
 //function to create holes row
@@ -94,6 +100,17 @@ function createCourse(course, teeSelectInput) {
 	table.append(hcp);
 	table.append(par);
 }
+// function to create player rows
+function createPlayers(players) {
+	for (let i = 0; i < players; i++) {
+		const playerEl = document.importNode(playerTemplate.content, true);
+		const tr = document.createElement('tr');
+		tr.classList.add(`player${i}`);
+		tr.append(playerEl);
+		table.append(tr);
+	}
+}
+
 // function to fetch data from api
 function getCourseInfo() {
 	const coursePromise = fetch('http://golf-courses-api.herokuapp.com/courses/11819').then((response) =>
