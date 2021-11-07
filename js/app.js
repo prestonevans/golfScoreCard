52134c8a532fc4e9d9bba342ece5f7eb95401f3b
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-07 10:00:54 -0700

    added teeSelect and logic for form

--- js/app.js
+++ js/app.js
@@ -3,5 +3,9 @@ form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const numberOfPlayers = document.querySelector('#numberOfPlayers');
 	const numberOfPlayersInput = numberOfPlayers.options[numberOfPlayers.selectedIndex].value;
+
+	const teeSelect = document.querySelector('#teeSelect');
+	const teeSelectInput = teeSelect.options[teeSelect.selectedIndex].value;
 	console.log(numberOfPlayersInput);
+	console.log(teeSelectInput);
 });
