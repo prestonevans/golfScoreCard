194087c86fc8d123f5b73de02b814806cad57f72
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-07 09:39:16 -0700

    added select for number of players and added logic to form

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const numberOfPlayers = document.querySelector('#numberOfPlayers');
+	const numberOfPlayersInput = numberOfPlayers.options[numberOfPlayers.selectedIndex].value;
+	console.log(numberOfPlayersInput);
+});
