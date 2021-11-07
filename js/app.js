9baab067b6967f46a7c81951d5750a5c6b46f15c
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-07 15:02:22 -0700

    made function to make rows for yards, hcp, and par. Also move get courseinfo call to top to not have to wait for submit to trigger api call

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
+// create variable to store golf course promise from function
+let coursePromise = getCourseInfo();
+
+//grab form to prevent defaulf from happening
 const form = document.querySelector('form');
+
+//grab table to append data
 const table = document.querySelector('table');
+
+//event listener to prevent the default of a form being submitted
 form.addEventListener('submit', (e) => {
+	// prevent form from default action
 	e.preventDefault();
+
+	//get the number of players selected from user
 	const numberOfPlayers = document.querySelector('#numberOfPlayers');
 	const numberOfPlayersInput = numberOfPlayers.options[numberOfPlayers.selectedIndex].value;
 
+	// get the tee the players will play from
 	const teeSelect = document.querySelector('#teeSelect');
 	const teeSelectInput = teeSelect.options[teeSelect.selectedIndex].value;
-	// console.log(numberOfPlayersInput);
-	// console.log(teeSelectInput);
 
+	//Creates holes row
+	createHoles();
+
+	// rended data from api
+	coursePromise.then((data) => {
+		const course = data.data;
+		createCourse(course, teeSelectInput);
+	});
+});
+//function to create holes row
+function createHoles() {
 	let tr = document.createElement('tr');
 	for (let i = 1; i <= 18; i++) {
 		let td = document.createElement('td');
@@ -37,4 +58,46 @@ form.addEventListener('submit', (e) => {
 		}
 	}
 	table.append(tr);
-});
+}
+// function to create yards, hcp, and par rows for table
+function createCourse(course, teeSelectInput) {
+	const yards = document.createElement('tr');
+	const hcp = document.createElement('tr');
+	const par = document.createElement('tr');
+	console.log(course);
+	for (let i = 0; i < 18; i++) {
+		let td = document.createElement('td');
+		if (i === 0) {
+			td.innerText = 'Yards';
+			td.colSpan = '2';
+			yards.append(td);
+			td = document.createElement('td');
+			td.innerText = 'HCP';
+			td.colSpan = '2';
+			hcp.append(td);
+			td = document.createElement('td');
+			td.innerText = 'PAR';
+			td.colSpan = '2';
+			par.append(td);
+		}
+		td = document.createElement('td');
+		td.innerText = course.holes[i].teeBoxes[teeSelectInput].yards;
+		yards.append(td);
+		td = document.createElement('td');
+		td.innerText = course.holes[i].teeBoxes[teeSelectInput].hcp;
+		hcp.append(td);
+		td = document.createElement('td');
+		td.innerText = course.holes[i].teeBoxes[teeSelectInput].par;
+		par.append(td);
+	}
+	table.append(yards);
+	table.append(hcp);
+	table.append(par);
+}
+// function to fetch data from api
+function getCourseInfo() {
+	const coursePromise = fetch('http://golf-courses-api.herokuapp.com/courses/11819').then((response) =>
+		response.json()
+	);
+	return coursePromise;
+}
